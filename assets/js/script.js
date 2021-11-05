b6bae1df35707d9ba85f17ec0b3c882fbc3eaeec
mahboba <misticse488@gmail.com>
2021-11-05 17:23:21 +0600

    5-11-2021/5:23pm

---
+++ assets/js/script.js
@@ -0,0 +1,20 @@
+jQuery(document).ready(function($){
+    $(".Homepage-slides").owlCarousel({
+        items:1,
+        loop:true,
+        nav:true,
+        dots:true,
+        navText:["<i class='fa fa-long-arrow-left'></i>" ,"<i class='fa fa-long-arrow-right'></i>"],
+        autoplay:false
+    
+
+
+    });
+
+
+});
+
+
+
+    $('.circlechart').circlechart();
+  
